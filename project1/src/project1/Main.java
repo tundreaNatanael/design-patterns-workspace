7cd10574c0cd8dc464d59418953d8e3d104ba4c0
nathan.tundrea <natanael.tundrea@emanuel.ro>
2025-11-06 11:42:33 +0200

    main decorator example added

--- project1/src/project1/Main.java
+++ project1/src/project1/Main.java
@@ -3,8 +3,17 @@ package project1;
 public class Main {
 
 	public static void main(String[] args) {
-		
+		DataSource apiDs = new ApiDataSource();
+		DataSource storageDs = new StorageDataSource();
+
+		apiDs.Write("something");
+		storageDs.Write("storage");
+
+		DataSource ecryptionDecorator = new EncryptionDecorator(storageDs);
+		ecryptionDecorator.Write("storage");
+
+		DataSource compressDecorator = new CompressDecorator(ecryptionDecorator);
+		compressDecorator.Write("storage");
 	}
-	
 
 }
