f37c82c941fddc9a7689e15f730ee2d7a8eed0c4
nathan.tundrea <natanael.tundrea@emanuel.ro>
2025-11-06 11:35:03 +0200

    clean project

--- project1/src/project1/Main.java
+++ project1/src/project1/Main.java
@@ -3,15 +3,7 @@ package project1;
 public class Main {
 
 	public static void main(String[] args) {
-		ClassicArtist beethoven = new ClassicArtist();
-		beethoven.singSong();
 		
-		RockArtist queen = new RockArtist();
-		queen.singSong();
-		
-		GeneralArtist generic = new GeneralArtist();
-		generic.singSong("classic");
-		generic.singSong("rock");
 	}
 	
 
